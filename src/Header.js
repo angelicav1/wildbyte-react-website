a0351cdf72f56ea8b9055a70460f74011c0bbbf6
Angelica Victoria <114718142+angelicav1@users.noreply.github.com>
2024-08-19 16:26:30 -0700

    added Header.js and header.css

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,11 @@ import "./header.css";
 export default function Header() {
     return (
         <div className="header">
-            Header
+            <div className="headerTitles">
+                <span className="headerTitleSmall">Wild Byte</span>
+                <span className="headerTitleLarge">Blog</span>
+            </div>
+            <img className="headerImage" src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/138/249/original/Yellowstone_mountains.jpg?1723263171" alt="Yellowstone Forest"></img>
         </div>
     )
 }
