b4798bb9624facd721dad9edee2937ce30edb2e9
Angelica Victoria <114718142+angelicav1@users.noreply.github.com>
2024-08-19 09:42:29 -0700

    added homepage features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import TopBar from "./TopBar";
+import Home from "./Home";
 
 export default function App() {
   return (
     <>
       <TopBar />
-     
+     <Home />
     </>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+import "./home.css";
+
+export default function Home() {
+    return (
+        <div className="home">
+            <Header />
+            Homepage
+        </div>
+    )
+}
