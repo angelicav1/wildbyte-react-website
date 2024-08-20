9bb5b95aaaf6475a295d121b58b1ff96b5fa7568
Angelica Victoria <114718142+angelicav1@users.noreply.github.com>
2024-08-19 20:41:05 -0700

    added Post.js and post.css

--- src/Post.js
+++ src/Post.js
@@ -3,8 +3,22 @@ import "./post.css";
 
 export default function Post() {
     return (
-        <div className="post">
-            post
+      <div className='post'>
+        <img
+          className='postImage'
+          src='https://s3.amazonaws.com/shecodesio-production/uploads/files/000/140/171/original/john-bell-haGopMEwC6M-unsplash.jpg?1724123504'
+          alt='Wild Coyote'
+        />
+        <div className='postInfo'>
+          <div className='postCategories'>
+            <span className='postCategories'>Animals</span>
+          </div>
+          <span className='postTitle'>
+            Coyote (Canis latrans)
+          </span>
+          <hr/>
+          <span className="postDate">1 hour ago</span>
         </div>
-    )
+      </div>
+    );
 }
