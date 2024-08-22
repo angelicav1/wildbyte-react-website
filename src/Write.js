fd7447307c554a8cd5c48a8cc75120797a04857b
Angelica Victoria <114718142+angelicav1@users.noreply.github.com>
2024-08-21 18:26:22 -0700

    updated Write.js and write.css

--- src/Write.js
+++ src/Write.js
@@ -4,18 +4,32 @@ import "./write.css";
 export default function Write() {
     return (
       <div className='write'>
+        <img
+          className='writeImage'
+          src='https://s3.amazonaws.com/shecodesio-production/uploads/files/000/140/171/original/john-bell-haGopMEwC6M-unsplash.jpg?1724123504'
+          alt='Wild Coyote'
+        />
         <form className='writeForm'>
           <div className='writeFormGroup'>
             <label htmlFor='fileInput'>
               <i className='writeIcon fa-solid fa-plus'></i>
             </label>
-            <input type='file' id='fileInput' style={{display:"none"}}/>
-            <input type='text' placeholder='Title' className="writeInput" autoFocus={true} />
+            <input type='file' id='fileInput' style={{ display: 'none' }} />
+            <input
+              type='text'
+              placeholder='Title'
+              className='writeInput'
+              autoFocus={true}
+            />
           </div>
-          <div className="writeFormGroup">
-            <textarea placeholder="Tell your story..." type="text" className="writeInput writeText"></textarea>
+          <div className='writeFormGroup'>
+            <textarea
+              placeholder='Tell your story...'
+              type='text'
+              className='writeInput writeText'
+            ></textarea>
           </div>
-          <button className="writeSubmit">Publish</button>
+          <button className='writeSubmit'>Publish</button>
         </form>
       </div>
     );
