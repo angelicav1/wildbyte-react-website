4eae9681c37ec5f7da0146226b2c5cfad8bbf055
Angelica Victoria <114718142+angelicav1@users.noreply.github.com>
2024-08-24 20:49:33 -0700

    updated App.js
    Added react-router-dom functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from "react";
 import TopBar from "./TopBar";
-import Register from "./Register";
+import Register from './Register';
+import Home from './Home';
+import Login from './Login';
+import Write from './Write';
+import Settings from './Settings';
+import Single from './Single';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 export default function App() {
+  const currentUser = true;
   return (
-    <>
+    <Router>
       <TopBar />
-     <Register />
-    </>
+      <Routes>
+        <Route path='/'>
+          <Home />
+        </Route>
+        <Route path='/posts'>
+          <Home />
+        </Route>
+        <Route path='/register'>{currentUser ? <Home /> : <Register />}</Route>
+        <Route path='/login'>{currentUser ? <Home /> : <Login />}</Route>
+        <Route path='/post/:id'>{<Single />}</Route>
+        <Route path='/write'> {currentUser ? <Write /> : <Login />}</Route>
+        <Route path='/settings'>
+          {' '}
+          {currentUser ? <Settings /> : <Login />}
+        </Route>
+      </Routes>
+    </Router>
   );
 }
