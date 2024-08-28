fd91d4016533729e074b2e143db22b89507b3205
Angelica Victoria <114718142+angelicav1@users.noreply.github.com>
2024-08-27 20:58:33 -0700

    added Mongo url

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-app.use("/",(reg,res)=>{
-    console.log("hey this is main url")
-})
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useCreateIndex: true,
+});
 
 app.listen("3000", () => {
     console.log("Backend is running")
