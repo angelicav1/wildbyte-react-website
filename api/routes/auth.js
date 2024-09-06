69d9c59d050f5fc9dff39cb25fdca743401ac045
Angelica Victoria <114718142+angelicav1@users.noreply.github.com>
2024-09-05 20:32:10 -0700

    added login async f(x)

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -22,3 +22,11 @@ router.post("/register", async (req,res)=>{
 })
 
 //LOGIN
+router.post("/login", async (req, res)=>{
+    try{
+        const user = User.findOne({username: req.body.username})
+    }catch(err){
+        res.status(500).json(err);
+    }
+    }
+})
