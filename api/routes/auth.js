caf98f557905cec664e9cd043f129f346fed6f8c
Angelica Victoria <114718142+angelicav1@users.noreply.github.com>
2024-09-06 21:12:22 -0700

    added Login queries

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -24,9 +24,15 @@ router.post("/register", async (req,res)=>{
 //LOGIN
 router.post("/login", async (req, res)=>{
     try{
-        const user = User.findOne({username: req.body.username})
+        const user = await User.findOne({username: req.body.username});
+        !user && res.status(400).json("Wrong credentials!");
+
+        const validated = await bcrypt.compare(req.body.password, user.password);
+        !validated && res.status(400).json("Wrong credentials!");
+
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
     }catch(err){
         res.status(500).json(err);
     }
-    }
-})
+});
